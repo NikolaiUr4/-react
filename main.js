289e1725cb45a555f71b3f3c56016e3c748e9323
NikolaiUr4 <nikolaiur4@gmail.com>
2020-05-02 15:01:40 +0700

    massage

---
+++ main.js
@@ -0,0 +1,39 @@
+/*let arr = [1,2,3,4,5,6,7,8,9,10]
+
+arr.forEach(   (item, id) => {
+    console.log(item)
+})
+console.log("-------------------------")
+arr.forEach(   (item, id) => {
+    if (id==4){
+        console.log(item)
+    }
+})
+console.log("-------------------------")
+arr.forEach( (item, id) => {
+    if(item%2){
+    console.log(item)
+    }
+*/});
+
+
+
+let arr = [-5, 12,-3,-2,0,16,-89,4,7,-145,5]
+
+/*arr.forEach((item, id) => {
+    if(item <-4 || (item > 7)) {
+        console.log(item)
+    }
+})
+let result = arr.map( (item, id) => {
+    if(item%2 == 0){
+        return Math.pow(item,2)
+    }
+    else return item
+});
+*/console.log(result)
+
+
+const sum = (x,y)=>{
+    return x+y
+}
