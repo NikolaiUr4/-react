9cc88011d6181bd5c08c1a146d9737a8fe8a9197
NikolaiUr4 <nikolaiur4@gmail.com>
2020-05-02 19:22:36 +0700

    lesson

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-/*let arr = [1,2,3,4,5,6,7,8,9,10]
+/*
+let arr = [1,2,3,4,5,6,7,8,9,10]
 
 arr.forEach(   (item, id) => {
     console.log(item)
@@ -14,13 +15,15 @@ arr.forEach( (item, id) => {
     if(item%2){
     console.log(item)
     }
-*/});
+});
+*/
 
 
 
+/*
 let arr = [-5, 12,-3,-2,0,16,-89,4,7,-145,5]
 
-/*arr.forEach((item, id) => {
+arr.forEach((item, id) => {
     if(item <-4 || (item > 7)) {
         console.log(item)
     }
@@ -31,9 +34,149 @@ let result = arr.map( (item, id) => {
     }
     else return item
 });
-*/console.log(result)
+console.log(result)
 
 
 const sum = (x,y)=>{
     return x+y
 }
+*/
+/*
+const sqrt = (x) => {
+    return x*x
+}
+
+console.log(sqrt(2))
+
+const parity = (x) => {
+    if(x%2 == 0){
+        console.log("Число чётное")
+    }
+    else console.log("Число нечётное")
+}
+
+parity(3)
+parity(4)
+
+const calc = (x, y, op) => {
+    switch(op){
+        case "+":
+            return x+y;
+            break;
+        case "-":
+            return x-y;
+            break;
+        case "*":
+            return x*y;
+            break;
+        case "/":
+            return x/y;
+            break;
+        default:
+            console.log("Error!")
+            break;
+    }
+}
+
+console.log(calc(2, 3, "+"));
+console.log(calc(2, 3, "-"));
+console.log(calc(2, 3, "*"));
+console.log(calc(2, 3, "/"));
+*/
+/*
+let arr = [1,2,3,4,5,6,7,8,9,10]
+const x = (a) => {
+    for(let i = 0; i < a.lenght; i++){
+        if(a[i]%2 == 0) {
+            console.log(a[i])
+        }
+    }    
+}
+x(arr);
+
+let arr2 = arr.filter((item, id) => {
+     return item%2 == 0
+})
+console.log(arr2) 
+
+function getInfo(name="%USER_NAME%", age="%USER_AGE%"){
+    console.log("Этого пользователя зовут " + name + " ему " + age + " лет")
+}
+getInfo("Петя", 140)
+getInfo("Петя")
+*/
+/*
+function f(...numbers){
+    console.log(numbers)
+}
+
+f(6,2,3,5,7,8,9,0,-4,5,3,1,"asd")
+*/
+/*
+function max(...numbers){
+    let tmpMax = numbers[0];
+    numbers.forEach((item, id) =>{
+        if(item > tmpMax){
+            tmpMax = item
+        }
+    })
+    console.log(tmpMax)
+}
+
+max(7,56,23,2134,7,8,4,234,1,-54,4)
+
+let arr = [24, 12464165451,-54459975]
+
+console.log(...arr, arr)
+*/
+
+/*
+const response = {
+    server: {
+        name: "Tusur.NET",
+        host: "88.204.74.108",
+        httpResponse:" игра была куплена",
+    },
+    user: "ulan@cmit.ru"
+}
+console.log(response.server)
+
+console.log(response.user + " вам пришел" + " от сервера " + response.server.name) 
+*/
+
+/*
+let school = {
+    name: "Школа",
+    number: "56",
+    getInfo:function(){
+        console.log("Динь-динь-динь, пора в " + this.name + " номер " +this.number)
+    }
+
+}
+
+school.getInfo()
+
+*/
+/*
+let student = {
+    firstName: "Петя",
+    secondName: "Петров"
+    //age: 5,
+    //getInfo: function(){
+    //    console.log("Меня зовут " + this.firstName)
+    //}
+}
+
+let {firstName: name, secondName} = student
+
+console.log(name, secondName)
+*/
+let connect = {
+    userName:"Admin",
+    port:  8000,
+    host: "127.0.0.1"
+}
+
+let = {userName: user,port: userPort,host: userHost} = connect
+
+console.log(user,userPort, userHost)
